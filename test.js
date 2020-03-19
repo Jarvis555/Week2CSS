71beed6cc48056c472e18a743745c7725a5df1cd
Jarvis555 <jarvisjackson591@gmail.com>
2020-03-19 09:16:55 -0500

    .

---
+++ test.js
@@ -0,0 +1,16 @@
+var message = "Hello,";
+
+if(message.length > 4){
+
+    message += " Dave.";
+
+}
+
+message = message + " I am Hal 9000.";
+
+console.log(message);
+
+
+var message = "Go hang a salami, I'm a lasagna hog.";
+
+console.log(message.substr(3,6));
